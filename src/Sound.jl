885343888635f98b14d5f3e93fb2cbd5a292eb91
Jeff Fessler <fessler@umich.edu>
2021-11-29 10:30:59 -0500

    support row vectors

--- src/Sound.jl
+++ src/Sound.jl
@@ -11,11 +11,25 @@ export sound, soundsc
 
 
 """
-    sound(x::AbstractVector, S::Real)
-Play monophonic audiosignal `x` at sampling rate `S` samples per second
+    sound(x::AbstractVector, S::Real = 8192)
+Play monophonic audio signal `x` at sampling rate `S` samples per second
 through default audio output device using the `PortAudio` package.
+Default sampling rate is `S = 8192` samples per second.
 """
-function sound(x::AbstractVector, S::Real)
+sound(x::AbstractVector, S::Real = 8192) = sound(reshape(x, :, 1), S)
+
+
+"""
+    sound(x::AbstractMatrix, S::Real = 8192)
+Play stereo audio signal `x` at sampling rate `S` samples per second
+through default audio output device using the `PortAudio` package.
+Default sampling rate is `S = 8192` samples per second.
+"""
+function sound(x::AbstractMatrix, S::Real = 8192)
+    if size(x,1) == 1 # row "vector"
+        x = vec(x) # convenience
+    end
+    size(x,2) ≤ 2 || throw("size(x,2) = $(size(x,2)) is too many channels")
     PortAudioStream(0, 2; samplerate=Float64(S)) do stream
         write(stream, x)
     end
@@ -23,46 +37,15 @@ end
 
 
 """
-    sound(x::AbstractMatrix, S::Real)
-Play stereo audiosignal `x` at sampling rate `S` samples per second
-through default audio output device using the `PortAudio` package.
-"""
-function sound(x::AbstractMatrix, S::Real)
-    size(x,2) == 2 || throw("size(x,2) = $(size(x,2)) is too many channels")
-    PortAudioStream(0, 2; samplerate=Float64(S)) do stream
-        write(stream, x)
-    end
-end
-
-
-"""
-    sound(x)
-Play mono or stereo audio signal `x`
-at default sampling rate 8192 samples per second
-through default audio output device using the `PortAudio` package.
-"""
-sound(x::AbstractArray) = sound(x, 8192)
-
-
-"""
-    soundsc(x, S::Real)
+    soundsc(x, S::Real = 8192)
 Play mono or stereo audio signal `x`
 at sampling rate `S` samples per second
 through default audio output device using the `PortAudio` package,
 after scaling `x` to have values in `(-1,1)`.
+Default sampling rate is `S = 8192` samples per second.
 """
-soundsc(x::AbstractArray, S::Real) =
+soundsc(x::AbstractArray, S::Real = 8192) =
     sound(x * prevfloat(1.0) / maximum(abs, x), S)
-
-
-"""
-    soundsc(x)
-Play mono or stereo audio signal `x`
-at default sampling rate 8192 samples per second
-through default audio output device using the `PortAudio` package,
-after scaling `x` to have values in `(-1,1)`.
-"""
-soundsc(x::AbstractArray) = soundsc(x, 8192)
 
 
 """
