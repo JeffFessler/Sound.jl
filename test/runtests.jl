0516a7e660b598abc03f14064edc9a05f49b608d
Jeff Fessler <fessler@umich.edu>
2022-02-14 20:34:45 -0500

    tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,28 @@
 # runtests.jl
 
-using Test: @test, @testset, @test_throws, detect_ambiguities
-using Sound # sound, soundsc, record
+using Test: @test, @testset, @test_throws, @inferred, detect_ambiguities
+using Sound # sound, soundsc, record, pick_output
 using SampledSignals: SampleBuf
-using PortAudio: devices
+using PortAudio: devices, PortAudioDevice
+
+@testset "output" begin
+    dev = @inferred sound(:first)
+    @test dev isa PortAudioDevice
+
+    index = findfirst(==(dev), devices())
+    io_out = IOBuffer()
+    io_in = IOBuffer("$index")
+    tmp = @inferred Sound.pick_output( ; io_in, io_out)
+    @test tmp === dev
+
+    io_in = IOBuffer("$index")
+    sound(:pick, randn(500), 1000; io_in, io_out)
+    sound(index, randn(500), 1000)
+    @test_throws String sound(0, randn(500), 1000)
+end
+
 
 @testset "Sound" begin
-
     S = 8192 # sampling rate in Hz
     x = 0.8 * cos.(2pi*(1:800)*440/S)
     y = 0.7 * sin.(2pi*(1:800)*660/S)
