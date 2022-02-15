dd33bb5a556fdef3f618c00526ed9bdedbc76326
Jeff Fessler <fessler@umich.edu>
2022-02-14 20:34:23 -0500

    Pick audio output device

--- src/soundsc.jl
+++ src/soundsc.jl
@@ -1,6 +1,6 @@
 # soundsc.jl
 
-using PortAudio: PortAudioStream, write, devices
+using PortAudio: PortAudioStream, PortAudioDevice, write, devices
 using Requires: @require
 import SignalBase: framerate
 
@@ -10,41 +10,110 @@ export sound, soundsc
 # fallback
 framerate(x::Any) = error("The signal you are trying to play as a sound ",
  "does not have a known sampling rate. ",
- "Consider wrapping in a `SampleBuf` or defining `framerate(x::MyType)`")
+ "Consider wrapping in a `SampleBuf` or defining `framerate(x::MyType)`.")
 
 
 """
-    sound(x::AbstractVector, S::Real = framerate(x))
+    pick_output( ; io_in::IO = stdin, io_out::IO = stdout)
+Show available audio devices and
+prompt user to select output device.
+"""
+function pick_output( ; io_in::IO = stdin, io_out::IO = stdout)
+    devs = devices()
+    outable = map(x -> x.output_bounds.max_channels > 0, devs)
+    any(outable) || throw("no output devices?")
+    devnum = findfirst(outable)
+    for (i,dev) in enumerate(devs)
+       println(io_out, outable[i] ? i : "?", " ", dev)
+    end
+    print(io_out, "pick output device [$devnum]: ")
+    char = read(io_in, Char)
+    if char == '\n'
+        return devs[devnum]
+    end
+    index = char - '0'
+    (1 ≤ index ≤ length(devs) && outable[index]) || throw("bad index $index")
+    return devs[index]
+end
+
+
+"""
+    sound(x::AbstractVector, S::Real = framerate(x), args...; kwargs...)
 Play monophonic audio signal `x` at sampling rate `S` samples per second
 through default audio output device using the `PortAudio` package.
 Caller must specify `S` unless a `framerate` method is defined for `x`.
 """
-sound(x::AbstractVector, S::Real = framerate(x)) = sound(reshape(x, :, 1), S)
+sound(x::AbstractVector, S::Real = framerate(x), args...; kwargs...) =
+    sound(reshape(x, :, 1), S, args...; kwargs...)
 
 
 """
-    sound(x::AbstractMatrix, S::Real = framerate(x))
+    sound(:pick, x, S::Real = framerate(x); kwargs...)
+Prompt user to pick output device, then play sound in `x`.
+"""
+sound(::Symbol,
+    x::AbstractArray,
+    S::Real = framerate(x) ;
+    io_in::IO = stdin,
+    io_out::IO = stdout,
+    kwargs...
+) = sound(x, S, pick_output( ; io_in, io_out); kwargs...)
+
+
+"""
+    sound(:first))
+Return first audio device that has output channels.
+"""
+sound(::Symbol) =
+    first(filter(x -> x.output_bounds.max_channels > 0, devices()))
+
+
+"""
+    sound(output_device_index::Int, x, S::Real = framerate(x); kwargs...)
+Play sound in `x` using `devices()[output_device_index]`.
+"""
+function sound(
+    index::Int,
+    x::AbstractArray,
+    S::Real = framerate(x) ;
+    kwargs...
+)
+    devs = devices()
+    (1 ≤ index ≤ length(devs)) || throw("bad index $index")
+    dev = devs[index]
+    dev.output_bounds.max_channels > 0 || throw("no output channels")
+    sound(x, S, dev; kwargs...)
+end
+
+
+"""
+    sound(x::AbstractMatrix, S::Real = framerate(x) [, output_device])
 Play stereo audio signal `x` at sampling rate `S` samples per second
 through default audio output device using the `PortAudio` package.
 Caller must specify `S` unless a `framerate` method is defined for `x`.
 """
-function sound(x::AbstractMatrix, S::Real = framerate(x))
+function sound(
+    x::AbstractMatrix,
+    S::Real = framerate(x),
+    output_device::PortAudioDevice = sound(:first),
+)
     if size(x,1) == 1 # row "vector"
         x = vec(x) # convenience
     end
     size(x,2) ≤ 2 || throw("size(x,2) = $(size(x,2)) is too many channels")
 
-    isempty(devices()) && (@warn("No audio devices."); return nothing)
+    output_device.output_bounds.max_channels > 0 ||
+        throw("no output channels for $output_device")
 
-    PortAudioStream(0, 2; samplerate=Float64(S)) do stream
+    PortAudioStream(output_device, 0, 2; samplerate=Float64(S)) do stream
         write(stream, x)
     end
 end
 
 
 """
-    soundsc(x, S::Real = framerate(x))
+    soundsc(x, S::Real = framerate(x), args...; kwargs...)
 Call `sound` after scaling `x` to have values in `(-1,1)`.
 """
-soundsc(x::AbstractArray, S::Real = framerate(x)) =
-    sound(x * prevfloat(1.0) / maximum(abs, x), S)
+soundsc(x::AbstractArray, S::Real = framerate(x), args...; kwargs...) =
+    sound(x * prevfloat(1.0) / maximum(abs, x), S, args...; kwargs...)
