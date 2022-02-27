c2879faa6681ede0974c7eaee52ee36df6261066
Jeff Fessler <fessler@umich.edu>
2022-02-26 22:58:03 -0500

    lit

--- docs/make.jl
+++ docs/make.jl
@@ -1,70 +1,76 @@
-using Sound
-using Documenter
-#using Literate
+execute = isempty(ARGS) || ARGS[1] == "run"
 
-# based on:
-# https://github.com/jw3126/UnitfulRecipes.jl/blob/master/docs/make.jl
+org, reps = :JeffFessler, :Sound
+eval(:(using $reps))
+using Documenter
+using Literate
 
 # https://juliadocs.github.io/Documenter.jl/stable/man/syntax/#@example-block
 ENV["GKSwstype"] = "100"
-
-# generate tutorials and how-to guides using Literate
-#lit = joinpath(@__DIR__, "lit")
-#src = joinpath(@__DIR__, "src")
-#notebooks = joinpath(src, "notebooks")
-
 ENV["GKS_ENCODING"] = "utf-8"
 
-DocMeta.setdocmeta!(Sound, :DocTestSetup, :(using Sound); recursive=true)
+# generate examples using Literate
+lit = joinpath(@__DIR__, "lit")
+src = joinpath(@__DIR__, "src")
+gen = joinpath(@__DIR__, "src/generated")
 
-#=
-execute = true # Set to true for executing notebooks and documenter!
-nb = false # Set to true to generate the notebooks
+base = "$org/$reps.jl"
+repo_root_url =
+    "https://github.com/$base/blob/main/docs/lit/examples"
+nbviewer_root_url =
+    "https://nbviewer.org/github/$base/tree/gh-pages/dev/generated/examples"
+binder_root_url =
+    "https://mybinder.org/v2/gh/$base/gh-pages?filepath=dev/generated/examples"
+
+
+repo = eval(:($reps))
+DocMeta.setdocmeta!(repo, :DocTestSetup, :(using $reps); recursive=true)
+
 for (root, _, files) in walkdir(lit), file in files
-    splitext(file)[2] == ".jl" || continue
+    splitext(file)[2] == ".jl" || continue # process .jl files only
     ipath = joinpath(root, file)
-    opath = splitdir(replace(ipath, lit=>src))[1]
-    Literate.markdown(ipath, opath, documenter = execute)
-    nb && Literate.notebook(ipath, notebooks, execute = execute)
+    opath = splitdir(replace(ipath, lit => gen))[1]
+    Literate.markdown(ipath, opath, documenter = execute; # run examples
+        repo_root_url, nbviewer_root_url, binder_root_url)
+    Literate.notebook(ipath, opath; execute = false, # no-run notebooks
+        repo_root_url, nbviewer_root_url, binder_root_url)
 end
-=#
+
 
 # Documentation structure
 ismd(f) = splitext(f)[2] == ".md"
 pages(folder) =
-    [joinpath(folder, f) for f in readdir(joinpath(src, folder)) if ismd(f)]
+    [joinpath("generated/", folder, f) for f in readdir(joinpath(gen, folder)) if ismd(f)]
 
 isci = get(ENV, "CI", nothing) == "true"
 
 format = Documenter.HTML(;
     prettyurls = isci,
     edit_link = "main",
-#   canonical = "https://JeffFessler.github.io/Sound.jl/stable",
-#   assets = String[],
+    canonical = "https://$org.github.io/$repo.jl/stable/",
+    assets = ["assets/custom.css"],
 )
 
 makedocs(;
-    modules = [Sound],
+    modules = [repo],
     authors = "Jeff Fessler and contributors",
-    sitename = "Sound.jl",
+    sitename = "$repo.jl",
     format,
     pages = [
         "Home" => "index.md",
         "Methods" => "methods.md",
- #      "Examples" => pages("examples")
+        "Examples" => pages("examples")
     ],
 )
 
 if isci
     deploydocs(;
-        repo = "github.com/JeffFessler/Sound.jl.git",
+        repo = "github.com/$base",
         devbranch = "main",
         devurl = "dev",
         versions = ["stable" => "v^", "dev" => "dev"],
         forcepush = true,
-    #   push_preview = true,
-        # see https://JeffFessler.github.io/Sound.jl/previews/PR##
+#       push_preview = true,
+        # see https://$org.github.io/$repo.jl/previews/PR##
     )
-else
-    @warn "may need to: rm -r src/examples"
 end
