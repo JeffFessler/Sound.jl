b3566d6ebba5ddfaa225231c11e777480d179c73
Jeff Fessler <fessler@umich.edu>
2022-04-02 09:54:33 -0400

    fix record example

--- examples/gtk-record.jl
+++ examples/gtk-record.jl
@@ -1,29 +1,32 @@
 #=
 gtk-record.jl
 Create a 3-button GUI with "record" "stop" "play" buttons
-to illustrate user control of recording.
-WIP!!
+to illustrate user control of audio recording with Gtk.
 =#
-using Gtk
+
+using Gtk: GtkGrid, GtkButton, GtkWindow, GAccessor
+using Gtk: GtkCssProvider, GtkStyleProvider
+using Gtk: set_gtk_property!, signal_connect, showall
 using PortAudio: PortAudioStream
 using Sound: sound
 
-# initialize global variables used throughout
+# initialize global variables that are used throughout
 const S = 44100 # sampling rate (samples/second)
 const N = 1024 # buffer length
-const maxtime = 300 # set maximum recording time to 5 minutes (300 seconds)
-const maxtime = 20 # set maximum recording time to 5 minutes (300 seconds)
+const maxtime = 10 # maximum recording time 10 seconds (for demo)
 recording = nothing # flag
-nsample = 0
+nsample = 0 # count number of samples recorded
+song = nothing # initialize "song"
 
-song = Float32[] # initialize "song" as an empty vector
 
 # callbacks
 
-# record until maximum duration is reached, or until the global "recording"
-# flag becomes false
+"""
+    record_loop!(in_stream, buf)
+Record from input stream until maximum duration is reached,
+or until the global "recording" flag becomes false.
+"""
 function record_loop!(in_stream, buf)
-println("in loop")
     global maxtime
     global S
     global N
@@ -31,49 +34,51 @@ println("in loop")
     global song
     global nsample
     Niter = floor(Int, maxtime * S / N)
-println("Niter $Niter")
+    println("\nRecording up to Niter=$Niter ($maxtime sec).")
     for iter in 1:Niter
-println("iter $iter")
         if !recording
-println("break")
             break
         end
-println("1")
         read!(in_stream, buf)
-println("2")
-        song[(iter-1)*N .+ (1:N)] = buf
-println("3")
+        song[(iter-1)*N .+ (1:N)] = buf # save buffer to song
         nsample += N
-#@show nsample
-println("nsample $nsample")
+        print("\riter=$iter/$Niter nsample=$nsample")
     end
     nothing
 end
 
+
+# callback function for "record" button
+# The @async below is important so that the Stop button can work!
 function call_record(w)
     global N
-    println("record")
-    in_stream = PortAudioStream(1, 0), # default input device
-println("buf0 N=$N")
-    buf = read(in_stream, N)
-println("buf1")
+    in_stream = PortAudioStream(1, 0) # default input device
+    buf = read(in_stream, N) # warm-up
     global recording = true
     global song = zeros(Float32, maxtime * S)
-println("loop")
-    record_loop!(in_stream, buf)
+    @async record_loop!(in_stream, buf)
     nothing
 end
 
+
+# callback function for "stop" button
 function call_stop(w)
     global recording = false
     global nsample
-    println("stop at nsample=$nsample")
+    duration = round(nsample / S, digits=2)
+    sleep(0.1) # ensure the async record loop finished
+    flush(stdout)
+    println("\nStop at nsample=$nsample, for $duration out of $maxtime sec.")
+    global song = song[1:nsample] # truncate song to the recorded duration
 end
 
+
+# callback function for "play" button
 function call_play(w)
-    println("play")
-#   sound(song[1:nsample], S) # play the entire recording
+    println("Play")
+    @async sound(song, S) # play the entire recording
 end
+
 
 g = GtkGrid() # initialize a grid to hold buttons
 set_gtk_property!(g, :column_spacing, 10) # gaps between buttons
@@ -99,3 +104,4 @@ bp = make_button("Play", call_play, 3, "wg", "color:white; background:green;")
 win = GtkWindow("gtk3", 600, 200) # 600×200 pixel window for all the buttons
 push!(win, g) # put button grid into the window
 showall(win) # display the window full of buttons
+nothing
